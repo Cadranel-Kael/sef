493dbcc6eeb1b838e571557bf5cec565ee0d86e3
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-07 21:02:03 +0200

    Init Commit

---
+++ web/app/themes/sef/resources/views/partials/nav.blade.php
@@ -0,0 +1,10 @@
+<nav class="navigation">
+  <h2 class="sr-only">{{ __('Menu de navigation') }}</h2>
+  <ul>
+    <li>
+      <a class="navigation__logo" href="{{ home_url('/') }}">
+        <img src="{{ asset('images/logo-sef.svg') }}" alt="{{ __('Retour vers la page d\'accueil') }}">
+      </a>
+    </li>
+  </ul>
+</nav>
