d81dae55cca9da201d21147c4f62a2260fc551cb
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-09 12:36:42 +0200

    Partials: Create main navigation partial

--- web/app/themes/sef/resources/views/partials/nav.blade.php
+++ web/app/themes/sef/resources/views/partials/nav.blade.php
@@ -1,10 +1,20 @@
-<nav class="navigation">
-  <h2 class="sr-only">{{ __('Menu de navigation') }}</h2>
-  <ul>
-    <li>
-      <a class="navigation__logo" href="{{ home_url('/') }}">
-        <img src="{{ asset('images/logo-sef.svg') }}" alt="{{ __('Retour vers la page d\'accueil') }}">
-      </a>
-    </li>
-  </ul>
+<nav class="nav">
+  <h2 class="nav__title sr-only">{{__('Menu principale')}}</h2>
+  <a title="{{ __('vers l\'accueil') }}" href="{{ home_url('/') }}">
+    {!! wp_get_attachment_image($siteLogo, 'thumbnail', false, ['class' => 'nav__logo']) !!}
+  </a>
+  @if (has_nav_menu('primary_navigation'))
+    <ul class="nav__menu">
+      @foreach(get_navigation_links('primary_navigation') as $link)
+        <li>
+          <a href="{{ $link->url }}" class="nav__link">{{ $link->label }}</a>
+        </li>
+      @endforeach
+    </ul>
+  @endif
+
+  @if($cta)
+    <x-button class="nav__button" type="primary" :href="$cta['url']">{{ $cta['title'] }}</x-button>
+  @endif
+  <x-search class="nav__search" />
 </nav>
