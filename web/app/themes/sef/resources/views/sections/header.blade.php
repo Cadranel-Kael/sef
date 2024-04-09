725788b725a6ba8f35f926bbd31da28e02a868af
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-09 12:35:59 +0200

    Section: Create header

--- web/app/themes/sef/resources/views/sections/header.blade.php
+++ web/app/themes/sef/resources/views/sections/header.blade.php
@@ -1,12 +1,4 @@
 <header class="header">
   @include('partials.nav')
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
-
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+  <span class="header__test">test</span>
 </header>
