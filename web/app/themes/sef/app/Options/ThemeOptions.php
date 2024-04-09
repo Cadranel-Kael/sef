33c2e01453b33a8a7a2715969308b62e4fe44039
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-09 12:39:44 +0200

    Add site logo

---
+++ web/app/themes/sef/app/Options/ThemeOptions.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Options;
+
+use Log1x\AcfComposer\Builder;
+use Log1x\AcfComposer\Options as Field;
+
+class ThemeOptions extends Field
+{
+    /**
+     * The option page menu name.
+     *
+     * @var string
+     */
+    public $name = 'Theme Options';
+
+    /**
+     * The option page document title.
+     *
+     * @var string
+     */
+    public $title = 'Theme Options | Options';
+
+    /**
+     * The option page field group.
+     */
+    public function fields(): array
+    {
+        $themeOptions = Builder::make('theme_options');
+
+        $themeOptions
+            ->addImage('site-logo', ['label' => 'Logo', 'return_format' => 'id'])
+            ->addLink('cta-nav-button', ['label' => 'Bouton de navigation CTA']);
+
+        return $themeOptions->build();
+    }
+}
