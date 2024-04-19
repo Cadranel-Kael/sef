7389264c3be019a6ccc7a6c72c1aec84be92e110
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-19 14:23:12 +0200

    .

--- web/app/themes/sef/resources/views/partials/nav.blade.php
+++ web/app/themes/sef/resources/views/partials/nav.blade.php
@@ -1,20 +1,28 @@
 <nav class="nav">
   <h2 class="nav__title sr-only">{{__('Menu principale')}}</h2>
-  <a title="{{ __('vers l\'accueil') }}" href="{{ home_url('/') }}">
-    {!! wp_get_attachment_image($siteLogo, 'thumbnail', false, ['class' => 'nav__logo']) !!}
-  </a>
+  <li>
+    <a class="nav__home" href="{{ home_url('/') }}">
+      {!! wp_get_attachment_image($siteLogo, 'thumbnail', false, ['class' => 'nav__logo']) !!}
+    </a>
+  </li>
   @if (has_nav_menu('primary_navigation'))
-    <ul class="nav__menu">
+    <x-burger-button id="burger-button"/>
+    <ul class="nav__menu" id="main-menu">
       @foreach(get_navigation_links('primary_navigation') as $link)
         <li>
           <a href="{{ $link->url }}" class="nav__link">{{ $link->label }}</a>
         </li>
       @endforeach
+      @if($cta)
+        <li>
+          <x-button class="nav__button" type="primary" :href="$cta['url']">{{ $cta['title'] }}</x-button>
+        </li>
+      @endif
+      <li class="nav__search-item">
+        <x-search class="nav__search"/>
+      </li>
     </ul>
   @endif
 
-  @if($cta)
-    <x-button class="nav__button" type="primary" :href="$cta['url']">{{ $cta['title'] }}</x-button>
-  @endif
-  <x-search class="nav__search" />
+
 </nav>
