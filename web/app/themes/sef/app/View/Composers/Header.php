4051422fd2c6cb8e877ba6c05939fe995fb8b70f
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-19 14:22:26 +0200

    Add composer for header

---
+++ web/app/themes/sef/app/View/Composers/Header.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Composers;
+
+use Roots\Acorn\View\Composer;
+
+class Header extends Composer
+{
+    /**
+     * List of views served by this composer.
+     *
+     * @var string[]
+     */
+    protected static $views = [
+        'sections.header'
+    ];
+
+    /**
+     * Data to be passed to view before rendering.
+     *
+     * @return array
+     */
+    public function with()
+    {
+        return [
+            'cta' => get_field('cta-nav-button', 'option')
+        ];
+    }
+}
