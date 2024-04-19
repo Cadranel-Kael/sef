9b5f4044c0ec68e8f61917b14499fd75b1f691ae
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-19 14:17:41 +0200

    Block: Teams

---
+++ web/app/themes/sef/app/Blocks/Teams.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Blocks;
+
+use Log1x\AcfComposer\Block;
+use Log1x\AcfComposer\Builder;
+use WP_Query;
+
+class Teams extends Block
+{
+    /**
+     * The block name.
+     *
+     * @var string
+     */
+    public $name = 'Teams';
+
+    /**
+     * The block description.
+     *
+     * @var string
+     */
+    public $description = 'A simple Teams block.';
+
+    /**
+     * The block category.
+     *
+     * @var string
+     */
+    public $category = 'formatting';
+
+    /**
+     * The block icon.
+     *
+     * @var string|array
+     */
+    public $icon = 'editor-ul';
+
+    /**
+     * The block keywords.
+     *
+     * @var array
+     */
+    public $keywords = [];
+
+    /**
+     * The block post type allow list.
+     *
+     * @var array
+     */
+    public $post_types = [];
+
+    /**
+     * The parent block type allow list.
+     *
+     * @var array
+     */
+    public $parent = [];
+
+    /**
+     * The ancestor block type allow list.
+     *
+     * @var array
+     */
+    public $ancestor = [];
+
+    /**
+     * The default block mode.
+     *
+     * @var string
+     */
+    public $mode = 'edit';
+
+    /**
+     * The default block alignment.
+     *
+     * @var string
+     */
+    public $align = '';
+
+    /**
+     * The default block text alignment.
+     *
+     * @var string
+     */
+    public $align_text = '';
+
+    /**
+     * The default block content alignment.
+     *
+     * @var string
+     */
+    public $align_content = '';
+
+    /**
+     * The supported block features.
+     *
+     * @var array
+     */
+    public $supports = [
+        'align' => true,
+        'align_text' => false,
+        'align_content' => false,
+        'full_height' => false,
+        'anchor' => false,
+        'mode' => false,
+        'multiple' => true,
+        'jsx' => true,
+    ];
+
+    /**
+     * The block styles.
+     *
+     * @var array
+     */
+    public $styles = [];
+
+    /**
+     * The block preview example data.
+     *
+     * @var array
+     */
+    public $example = [
+        'items' => [
+            ['item' => 'Item one'],
+            ['item' => 'Item two'],
+            ['item' => 'Item three'],
+        ],
+    ];
+
+    /**
+     * The block template.
+     *
+     * @var array
+     */
+    public $template = [
+        'core/heading' => ['placeholder' => 'Hello World'],
+        'core/paragraph' => ['placeholder' => 'Welcome to the Teams block.'],
+    ];
+
+    /**
+     * Data to be passed to the block before rendering.
+     */
+    public function with(): array
+    {
+        return [
+            'heading' => $this->heading(),
+            'join_us' => $this->joinUs(),
+            'teams' => $this->getTeams(),
+        ];
+    }
+
+    /**
+     * The block field group.
+     */
+    public function fields(): array
+    {
+        $teams = Builder::make('teams');
+
+        $teams
+            ->addWysiwyg('heading')
+            ->addText('join_us');
+
+        return $teams->build();
+    }
+
+    /**
+     * Retrieve the heading.
+     *
+     * @return array
+     */
+    public function heading()
+    {
+        return get_field('heading', false, false);
+    }
+
+    /**
+     * Retrieve the join.
+     *
+     * @return array
+     */
+    public function joinUs()
+    {
+        return get_field('join_us');
+    }
+
+    /**
+     * Retrieve the join.
+     *
+     * @return WP_Query
+     */
+    public function getTeams()
+    {
+//        $args = array(
+//            'posts_per_page'   => -1,
+//            'post_type'        => 'teams',
+//            'order' => 'ASC',
+//            'post_status' => 'publish',
+//        );
+//        return new WP_Query($args);
+        $args = array(
+            'posts_per_page'   => -1,
+            'post_type'        => 'teams',
+            'order'            => 'ASC',
+            'post_status'      => 'publish',
+        );
+        return new WP_Query($args);
+    }
+
+
+    /**
+     * Assets enqueued when rendering the block.
+     */
+    public function assets(array $block): void
+    {
+        //
+    }
+}
---
+++ web/app/themes/sef/resources/views/blocks/teams.blade.php
@@ -0,0 +1,28 @@
+<section class="teams">
+  <h2 class="teams__title">{!! $heading !!}</h2>
+  @while($teams->have_posts())
+    @php($teams->the_post())
+    @php($members = get_field('member', get_the_ID()))
+    <div class="teams__team team">
+      <h3 class="team__title">{!! get_the_title() !!}</h3>
+      @if($members)
+        <ul class="team__members">
+          @foreach($members as $member)
+            <li class="team__member member">
+              {!! wp_get_attachment_image($member['image'], 'thumbnail', false, ['class' => 'member__image']) !!}
+              <div class="member__name">{{ $member['full_name'] }}</div>
+            </li>
+          @endforeach
+        </ul>
+      @else
+        <div>{{ __('Il y a aucun membre dans cette equipe') }}</div>
+      @endif
+    </div>
+  @endwhile
+
+  <div class="teams__container">
+    <div class="team__join">{{ $join_us }}</div>
+    <x-button type="primary">Nous contacter</x-button>
+    <x-button type="primary">Devenir bénévole</x-button>
+  </div>
+</section>
