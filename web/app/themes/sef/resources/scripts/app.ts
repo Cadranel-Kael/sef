a667921bfe1a3c8aa422c152f435a2135ceacb9d
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-19 14:19:57 +0200

    .

--- web/app/themes/sef/resources/scripts/app.ts
+++ web/app/themes/sef/resources/scripts/app.ts
@@ -1,5 +1,7 @@
 import domReady from '@roots/sage/client/dom-ready';
 import {Search} from "./classes/Search";
+import {BurgerMenu} from "./classes/BurgerMenu";
+import {SlideSide} from "./classes/SlideSide";
 
 /**
  * Application entrypoint
@@ -7,6 +9,8 @@ import {Search} from "./classes/Search";
 domReady(async () => {
   const search = document.querySelector('.search') as HTMLElement;
   new Search(search, search.querySelector('#form'), search.querySelector('#search') as HTMLInputElement, search.querySelector('#button') as HTMLButtonElement);
+  new BurgerMenu(document.querySelector('#main-menu') as HTMLUListElement, document.querySelector('#burger-button') as HTMLButtonElement);
+  document.querySelectorAll('[x-slide]').forEach((element) => new SlideSide(element));
 });
 
 /**
