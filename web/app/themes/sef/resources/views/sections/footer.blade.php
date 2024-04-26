3a5e438f94c9bb7700fce7b7610aa51bb848c329
kael-cadranel <kael.cadranel00@gmail.com>
2024-04-26 13:28:01 +0200

    Create footer

--- web/app/themes/sef/resources/views/sections/footer.blade.php
+++ web/app/themes/sef/resources/views/sections/footer.blade.php
@@ -1,3 +1,23 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="footer">
+  <nav>
+    <h2 class="sr-only">{{__('Menu footer')}}</h2>
+    <a class="footer__home" href="{{ home_url('/') }}">
+      {!! wp_get_attachment_image($siteLogo, 'thumbnail', false, ['class' => 'footer__logo style-svg']) !!}
+    </a>
+    @if (has_nav_menu('footer_navigation'))
+      <ul class="footer__menu" id="footer-menu">
+        @foreach(get_navigation_links('footer_navigation') as $link)
+          <li>
+            <a href="{{ $link->url }}" class="footer__link @if($link->active) footer__link--active @endif">{{ $link->label }}</a>
+          </li>
+        @endforeach
+      </ul>
+    @endif
+  </nav>
+  <div class="footer__info">
+    <p class="footer__text">{{ $phone }}</p>
+    <p class="footer__text">{{ $email }}</p>
+    <p class="footer__text">{{ $address }}</p>
+  </div>
+  <x-button>Nous soutenir</x-button>
 </footer>
