bad77c1d38a2e744dbe332a5263298e032d95430
kael-cadranel <kael.cadranel00@gmail.com>
2024-06-05 14:54:08 +0200

    Add image

--- web/app/themes/sef/resources/views/blocks/paragraphs.blade.php
+++ web/app/themes/sef/resources/views/blocks/paragraphs.blade.php
@@ -2,8 +2,13 @@
   @while( have_rows('blocks') )
     @php(the_row())
     <article class="paragraphs__block">
-      <h2 class="paragraphs__title">{!! get_sub_field('heading') !!}</h2>
-      <p class="paragraphs__text">{!! get_sub_field('text', false, false) !!}</p>
+    @if(get_sub_field('image_field'))
+      {!! wp_get_attachment_image(get_sub_field('image'), 'medium', false, ['class' => 'paragraphs__image']) !!}
+    @endif
+      <div class="paragraphs__text">
+        <h2 class="paragraphs__title">{!! get_sub_field('heading') !!}</h2>
+        <p class="paragraphs__paragraph">{!! get_sub_field('text', false, false) !!}</p>
+      </div>
     </article>
   @endwhile
 </div>
