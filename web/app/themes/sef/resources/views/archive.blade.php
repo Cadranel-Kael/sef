d3b8849a0a18fe64f51f3a492419554ac055ff61
kael-cadranel <kael.cadranel00@gmail.com>
2024-06-08 14:34:21 +0200

    Remove typo

--- web/app/themes/sef/resources/views/archive.blade.php
+++ web/app/themes/sef/resources/views/archive.blade.php
@@ -1,4 +1,4 @@
-I@extends('layouts.app', ['title' => $title])
+@extends('layouts.app', ['title' => $title])
 
 @section('content')
   @include('partials.archives.archive-' . get_queried_object()->name)
