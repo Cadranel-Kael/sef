eaf004b6153a84d44f34a3c46ad64b645d8b63ac
kael-cadranel <kael.cadranel00@gmail.com>
2024-06-08 13:35:43 +0200

    Add error when no houses posts exists

--- web/app/themes/sef/resources/views/partials/partials/archives/archive-houses.blade.php
+++ web/app/themes/sef/resources/views/partials/partials/archives/archive-houses.blade.php
@@ -1,11 +1,14 @@
 <div class="houses">
-  <h2 class="houses__title">Nos <strong>hébergements</strong></h2>
+  <div class="houses__title">Nos <strong>hébergements</strong></div>
   <div class="houses__container">
+    @if($houses->isEmpty())
+      Il y a pas d'hébergements a afficher.
+    @endif
     @foreach($houses as $house)
       <article class="houses__house house">
         {!! wp_get_attachment_image($house->image, 'medium', false, ['class' => 'house__image']) !!}
         <div class="house__container">
-          <h3 class="house__title">{{ $house->title }}</h3>
+          <h2 class="house__title">{{ $house->title }}</h2>
           <ul class="house__attributes">
             <li class="house__places">{{ $house->places }} personnes par ans</li>
             <li class="house__beds">{{ $house->beds }} lits en hebergements d’urgence</li>
