876e092c929aa6843bc2da87ffa9da323a546791
kael-cadranel <kael.cadranel00@gmail.com>
2024-06-09 12:31:55 +0200

    Add styling to shop archive

--- web/app/themes/sef/resources/views/partials/archives/archive-shops.blade.php
+++ web/app/themes/sef/resources/views/partials/archives/archive-shops.blade.php
@@ -3,12 +3,12 @@
   <p class="shops__description">Nous avons plusieurs magasins partenaire qui finance notre cause. Ceci est un moyen de nous soutenire.</p>
   <div class="shops__container">
       @foreach($shops as $shop)
-        <article class="shops__shop shop">
+        <article class="shops__card card">
           {!! wp_get_attachment_image($shop->image, 'medium', false, ['class' => 'shop__image']) !!}
-          <h2 class="shop__title">{{ $shop->title }}</h2>
-          <p class="shop__type">{{ $shop->type }}</p>
-          <p class="shop__address">{{ $shop->location }}</p>
-          <a class="shop__link" href="{{ $shop->link }}">Vers la page {{ $shop->title }}</a>
+          <h2 class="card__title">{{ $shop->title }}</h2>
+          <p class="card__type">{{ $shop->type }}</p>
+          <p class="card__address">{{ $shop->location }}</p>
+          <a class="card__link" href="{{ $shop->link }}">Vers la page {{ $shop->title }}</a>
         </article>
       @endforeach
   </div>
