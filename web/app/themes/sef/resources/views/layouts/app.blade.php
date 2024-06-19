f462700a46e41c9b20baf5af303d87ed296f3ade
kael-cadranel <kael.cadranel00@gmail.com>
2024-06-19 07:03:47 +0200

    .

--- web/app/themes/sef/resources/views/layouts/app.blade.php
+++ web/app/themes/sef/resources/views/layouts/app.blade.php
@@ -1,36 +1,36 @@
 <!doctype html>
 <html @php(language_attributes())>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @php(do_action('get_header'))
-    @php(wp_head())
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @php(do_action('get_header'))
+  @php(wp_head())
+</head>
 
-  <body @php(body_class())>
-    @php(wp_body_open())
+<body @php(body_class())>
+@php(wp_body_open())
 
-    <div class="app" id="app">
-      <a class="sr-only focus:not-sr-only" href="#main">
-        {{ __('Skip to content') }}
-      </a>
-      <h1 class="sr-only">{{ $title ?? get_the_title() }}</h1>
+<div class="app" id="app">
+  <a class="sr-only focus:not-sr-only" href="#main">
+    {{ __('Skip to content') }}
+  </a>
+  <h1 class="sr-only">{{ $title ?? get_the_title() }}</h1>
 
-      @include('sections.header')
+  @include('sections.header')
 
-      <div class="fixed-cta" data-align="right" x-slide>
-        <x-button type="primary" class="fixed-cta__button" :href="$btn1['url']">{{ $btn1['title'] }}</x-button>
-        <x-button type="outline" class="fixed-cta__button" :href="$btn2['url']">{{ $btn2['title'] }}</x-button>
-      </div>
+  <div class="fixed-cta" data-align="right" x-slide>
+    <x-button type="primary" class="fixed-cta__button" :href="$btn1['url']">{{ $btn1['title'] }}</x-button>
+    <x-button type="outline" class="fixed-cta__button" :href="$btn2['url']">{{ $btn2['title'] }}</x-button>
+  </div>
 
-      <main id="main" class="app__main">
-        @yield('content')
-      </main>
+  <main id="main" class="app__main">
+    @yield('content')
+  </main>
 
-      @include('sections.footer')
-    </div>
+  @include('sections.footer')
+</div>
 
-    @php(do_action('get_footer'))
-    @php(wp_footer())
-  </body>
+@php(do_action('get_footer'))
+@php(wp_footer())
+</body>
 </html>
