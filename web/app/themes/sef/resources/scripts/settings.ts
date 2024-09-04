266894ed6fc0a2d22a59e96a5913282e5b202044
kael-cadranel <kael.cadranel00@gmail.com>
2024-09-04 11:04:15 +0200

    add settings

---
+++ web/app/themes/sef/resources/scripts/settings.ts
@@ -0,0 +1,14 @@
+export const settings = {
+  animation: {
+    onAppear: {
+      className: 'on-appear',
+      hiddenClass: 'on-appear--hidden',
+      visibleClass: 'on-appear--visible',
+    },
+  },
+  numberAnimator: {
+    className: 'number-animator',
+    hiddenClass: 'number-animator--hidden',
+    visibleClass: 'number-animator--visible',
+  },
+}
